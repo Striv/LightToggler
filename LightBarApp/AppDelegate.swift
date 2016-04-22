5cf6b41f6facb1d1a4171d2d9c5851016b3cf51e
Daniel Strivelli <Dan@n.local>
2016-04-21 23:01:24 -0400

    Initial Commit

---
+++ LightBarApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LightBarApp
+//
+//  Created by Daniel Strivelli on 4/21/16.
+//  Copyright © 2016 DanStrivelli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
